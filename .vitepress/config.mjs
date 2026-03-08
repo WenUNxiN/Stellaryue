dba42b6b6f24f3dbd74fa9adbf0d33e94e74be15
WenUNxiN <2627719259@qq.com>
2026-03-08 21:32:06 +0800

    tj

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -25,5 +25,11 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/WenUNxiN' }
     ]
+  },
+
+  vite: {
+    ssr: {
+      noExternal: ['@escook/vitepress-theme', 'vitepress']
+    }
   }
 })
